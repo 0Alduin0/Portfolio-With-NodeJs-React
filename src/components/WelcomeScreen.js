0e783541ed1a87aa1c1620c9d9c5a59364cec975
Enes Yürekli <muhammedenesyurekli@gmail.com>
2025-05-23 17:21:08 +0300

    giriş ekranı rengi ayarlandı

--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -31,8 +31,8 @@ const TypewriterEffect = ({ text }) => {
 
 const BackgroundEffect = () => (
   <div className="absolute inset-0 overflow-hidden">
-    <div className="absolute inset-0 bg-gradient-to-r from-indigo-600/20 to-purple-600/20 blur-3xl animate-pulse" />
-    <div className="absolute inset-0 bg-gradient-to-tr from-indigo-600/10 via-transparent to-purple-600/10 blur-2xl animate-float" />
+    <div className="absolute inset-0 bg-gradient-to-r from-purple-600/20 to-purple-600/20 blur-3xl animate-pulse " />
+    <div className="absolute inset-0 bg-gradient-to-tr from-red-600/10 via-transparent to-red-600/10 blur-2xl animate-float" />
   </div>
 );
 
